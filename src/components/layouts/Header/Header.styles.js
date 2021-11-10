89ed4c4be724b459df574a8f2f58da4109b30b4c
armistrice <egor.leineveber@altence.com>
2021-11-10 23:25:53 +0300

    fix(header): fix style

--- src/components/layouts/Header/Header.styles.js
+++ src/components/layouts/Header/Header.styles.js
@@ -1,8 +1,11 @@
 import { Typography } from 'antd';
 import styled from 'styled-components';
+import { hexToRGB } from 'utils/utils';
 
 export const UsernameText = styled(Typography.Text)`
   color: ${(props) => props.theme.colors.text.secondary};
 `;
 
-export const OnlineText = styled(Typography.Text)``;
+export const OnlineText = styled(Typography.Text)`
+  color: ${(props) => hexToRGB(props.theme.colors.text.main, 0.75)};
+`;
