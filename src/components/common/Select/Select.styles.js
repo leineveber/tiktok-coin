6bd0f4dc1acc2533a79d2139bf2584c7e9b67a5e
armistrice <egor.leineveber@altence.com>
2021-11-13 19:17:23 +0300

    fix(select): use standard select instead of antd select

--- src/components/common/Select/Select.styles.js
+++ src/components/common/Select/Select.styles.js
@@ -1,8 +1,24 @@
-import { Select as AntSelect } from 'antd';
 import styled from 'styled-components';
 
-export const Select = styled(AntSelect)`
-  &:not(.ant-select-customize-input) .ant-select-selector {
-    background: #f2f3f5;
+export const Select = styled.select`
+  width: 100%;
+  padding: 0 11px;
+  height: 42px;
+  background: #f2f3f5;
+  border: 1px solid #d9d9d9;
+  transition: all 0.3s ease;
+
+  border-radius: ${(props) => props.theme.border.base};
+
+  &:hover {
+    border-color: #8597ff;
+    border-right-width: 1px !important;
+  }
+
+  &:focus {
+    border-color: #8597ff;
+    border-right-width: 1px !important;
+    outline: 0;
+    box-shadow: 0 0 0 2px rgb(91 109 255 / 20%);
   }
 `;
