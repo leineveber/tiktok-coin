2a519197d5891f37c2d542a9fd6af150f7a9a7d4
armistrice <egor.leineveber@altence.com>
2021-11-13 16:14:35 +0300

    fix(drawer): fix swipes

--- src/components/common/Drawer/Drawer.styles.js
+++ src/components/common/Drawer/Drawer.styles.js
@@ -11,6 +11,10 @@ export const Drawer = styled(AntDrawer)`
 `;
 
 export const SwipeWrapper = styled.div`
-  margin: -24px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   padding: 24px;
 `;
