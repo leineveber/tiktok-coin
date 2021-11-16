042370574db318a96234340ca5568e13e31a93ba
armistrice <egor.leineveber@altence.com>
2021-11-16 22:01:22 +0300

    fix(maincard): make icons with static position

--- src/components/common/cards/MainCard/MainCard.jsx
+++ src/components/common/cards/MainCard/MainCard.jsx
@@ -16,34 +16,37 @@ export const MainCard = ({
   onClick,
 }) => (
   <S.MainCard className={className} background={background} isPrimary={isPrimary} onClick={onClick}>
-    <Row justify="space-between" align="middle" gutter={[0, 0]}>
-      <Col span={24}>
-        <S.Title>{title}</S.Title>
+    <Row justify="space-between" align="middle" gutter={[0, 0]} wrap={false}>
+      <Col>
+        <Col span={24}>
+          <S.Title>{title}</S.Title>
+        </Col>
+        <Col span={24}>
+          <Row gutter={[0, 5]}>
+            <Col span={24}>
+              <S.Description>{description}</S.Description>
+
+              {text && <S.Text>{text}</S.Text>}
+            </Col>
+
+            <Col span={24}>
+              <Row gutter={[20, 0]}>
+                <Col>
+                  <Button type="default">{btnText}</Button>
+                </Col>
+
+                {secondBtn && <Col>{secondBtn}</Col>}
+              </Row>
+            </Col>
+          </Row>
+        </Col>
       </Col>
 
-      <Col span={24}>
-        <Row gutter={[0, 5]}>
-          <Col span={24}>
-            <S.Description>{description}</S.Description>
-
-            {text && <S.Text>{text}</S.Text>}
-          </Col>
-
-          <Col span={24}>
-            <Row gutter={[20, 0]}>
-              <Col>
-                <Button type="default">{btnText}</Button>
-              </Col>
-
-              {secondBtn && <Col>{secondBtn}</Col>}
-            </Row>
-          </Col>
-        </Row>
+      <Col>
+        <S.ImgWrapper>
+          <Image src={icon} alt="Icon" preview={false} />
+        </S.ImgWrapper>
       </Col>
-
-      <S.ImgWrapper>
-        <Image src={icon} alt="Icon" preview={false} />
-      </S.ImgWrapper>
     </Row>
   </S.MainCard>
 );
